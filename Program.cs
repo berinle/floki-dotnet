2cf202fc66a73c8741fad9055832b4ccc9d0283d
Bayo Erinle <berinle@gmail.com>
2025-02-26 15:36:31 -0500

    added logging statements

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Npgsql;
 using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddFilter("Npgsql", LogLevel.Trace); // Set Npgsql logging level to Debug
 
 // Parse VCAP_SERVICES to get PostgreSQL credentials
 var vcapServices = Environment.GetEnvironmentVariable("VCAP_SERVICES");
@@ -42,13 +48,18 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 // Add an API endpoint to fetch PostgreSQL version
-app.MapGet("/api/version", async () =>
+app.MapGet("/api/version", async (ILogger<Program> logger) =>
 {
     await using var conn = new NpgsqlConnection(connectionString);
     await conn.OpenAsync();
 
+    // Log a statement after establishing the connection
+    logger.LogInformation("Database connection established successfully.");
+
     await using var cmd = new NpgsqlCommand("SELECT version()", conn);
+    logger.LogInformation("Query has been successfully constructed");
     var version = await cmd.ExecuteScalarAsync();
+    logger.LogInformation("Query executed successfully.");
 
     return new { Version = version?.ToString() };
 });
--- readme.md
+++ readme.md
@@ -6,11 +6,28 @@
 - [.NET 8 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/8.0) (at minimum. tested with 8.0.8)
 - [VCAP_SERVICES](https://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#VCAP-SERVICES)
 
+## Running locally
+
+```bash
+cp .env.example .env
+# update .env with your own values
+
+dotnet run
+```
+
 ## Deploying
 
 ```bash
 cf create-user-provided-service mypg -p '{"host":"your_host", "database":"your_db", "username":"your_username", "password":"your_password", "port":5432}'
+
 cf push
+```
+
+## Building & deploying as a self-contained deployment
+```sh
+dotnet publish -c Release -r linux-x64 --self-contained true -o ./publish
+
+cf push <new app name>-p ./publish
 ```
 
 ## Screenshot
